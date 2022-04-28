1a1223880343c0bb5eccfa2b1349aa6d634b0f0e
saphalpathak <saphalpathak3@gmail.com>
2022-04-28 16:23:52 +0545

    api testing

--- src/main/java/com/texas/studentstatus/StudentStatusSystemApplication.java
+++ src/main/java/com/texas/studentstatus/StudentStatusSystemApplication.java
@@ -1,7 +1,6 @@
 package com.texas.studentstatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
