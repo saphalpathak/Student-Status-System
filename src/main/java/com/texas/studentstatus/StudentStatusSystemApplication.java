0ed22b3a08e11a483b186ab8c1539c5b74db4cfc
saphalpathak <saphalpathak3@gmail.com>
2022-04-28 16:19:30 +0545

    make it better

--- src/main/java/com/texas/studentstatus/StudentStatusSystemApplication.java
+++ src/main/java/com/texas/studentstatus/StudentStatusSystemApplication.java
@@ -1,6 +1,4 @@
 package com.texas.studentstatus;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
